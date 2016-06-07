420521f0af297b4beb9f0cbe04cd05c220a44b2b
王志准 <1986260711@qq.com>
2016-06-07 22:23:42 +0800

    第一次新建

---
+++ qqqqqqq/qqqqqqq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qqqqqqq
+//
+//  Created by 王志准 on 16/6/7.
+//  Copyright © 2016年 王志准. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ qqqqqqq/qqqqqqq/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  qqqqqqq
+//
+//  Created by 王志准 on 16/6/7.
+//  Copyright © 2016年 王志准. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
