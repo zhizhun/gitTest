7f813fdb0cd406cd0d9f2148e2a6e7a4158125ac
王志准 <1986260711@qq.com>
2016-06-07 22:46:02 +0800

    sec

---
+++ qqqqqqq/qqqqqqq/secViewController.h
@@ -0,0 +1,13 @@
+//
+//  secViewController.h
+//  qqqqqqq
+//
+//  Created by 王志准 on 16/6/7.
+//  Copyright © 2016年 王志准. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface secViewController : UIViewController
+
+@end
